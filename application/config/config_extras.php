ebca9518690caf251024889482b86f4da15582e2
Solucionaticos <solucionaticos@gmail.com>
2019-08-20 20:27:47 -0500

    reCaptcha
    Credenciales nuevas

--- application/config/config_extras.php
+++ application/config/config_extras.php
@@ -10,16 +10,23 @@ $config['version'] = '1.0.0.0';
 $config['YmdHis']  = date('Y-m-d H:i:s'); // date('Y-m-d H:i:s', strtotime('+2 hours'));
 
 // Email
+/*
 $config['email_smtp_host'] = 'smtp.sendgrid.net';
 $config['email_smtp_user'] = 'apikey';
 $config['email_smtp_pass'] = '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';
 $config['email_smtp_port'] = '587';
-$config['email_smtp_user_name'] = 'Architekti ve škole';
-$config['email'] = 'info@architektiveskole.cz'; 
+*/
+$config['email_smtp_host'] = '';
+$config['email_smtp_user'] = '';
+$config['email_smtp_pass'] = '';
+$config['email_smtp_port'] = '';
+
+$config['email_smtp_user_name'] = 'Prsteny Baroň';
+$config['email'] = 'info@prstenybaron.cz'; 
 
 // Recaptcha
-$config['recaptcha_sitekey'] = '6LfdUaAUAAAAAIcnHKlrpsuFjQkImTeFXgDa668Q';
-$config['recaptcha_secret'] = '6LfdUaAUAAAAAIv6fwssfnIPABxpXQUmMrVnXOsz';
+$config['recaptcha_sitekey'] = '6LfQBrQUAAAAAGcHc-JDI4y39VOtUrXlQ4fz4aVj';
+$config['recaptcha_secret'] = '6LfQBrQUAAAAAB3CIXuFCwmK1QX7IAyNxb9H7rv6';
 
 // Blogs
 $config['content_list_num_characters_home'] = 100;
