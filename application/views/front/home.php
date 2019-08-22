a1f9b27aea4a05d1261de60b0e930298bfa66635
hamely <sarmiento101147@gmail.com>
2019-08-22 15:39:10 -0500

    modal pedido
    diseño

--- application/views/front/home.php
+++ application/views/front/home.php
@@ -41,7 +41,7 @@
 								</div>
 						
 								<div style="background:white; text-align:center; margin-top:10px;">
-								<a href="#" class="buy-now text-center py-2 btn btn-light btn-sm" style="margin-left: 4px;">OBJEDNÁVKOVÝ FORMULÁR<span><i class="ion-ios-cart ml-1"></i></span></a>
+								<a href="#" class="buy-now text-center py-2 btn btn-light btn-sm" style="margin-left: 4px;" data-toggle="modal" data-target="#orderModal">OBJEDNÁVKOVÝ FORMULÁR<span><i class="ion-ios-cart ml-1"></i></span></a>
 								</div>
 								
 		    				</div>
@@ -69,7 +69,8 @@
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
-            </div>
+			</div>
+			<div class="container">
             <div class="modal-body">
                 <form>
 					<label for="inputEmail4">NEZÁVAZNÝ VÝPOčET CENY</label>
@@ -85,6 +86,9 @@
                         <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
                       </div>
 					</div>
+
+					
+
 					<label for="inputEmail4">TYP PRSTENU</label>
 					<div class="form-row">
 					
@@ -111,6 +115,8 @@
 						<input type="password" class="form-control" id="inputPassword4" placeholder="">
 						</div>
 					  </div>
+
+					 
 
 					<label for="inputEmail4">TYP KAMENE</label>  
 					<div class="form-check">
@@ -159,14 +165,199 @@
 							</label>
 					</div>
 					<div class="form-group">
-  <label for="usr">Name:</label>
-  <input type="text" class="form-control" id="usr">
-</div>
+						<label for="usr">počet kamenů:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+
+					<span> Nejpozději do druhého dne Vám bude odeslána cenová nabídka.</span>
+					<span> 	Nezávazný výpočet ceny lze také provést telefonicky: 732 450 054.</span>
+				
+					<div class="form-group">
+						<label for="message-text" class="col-form-label">Vaše poznámky, vlastní úpravy:</label>
+						<textarea class="form-control" id="message-text"></textarea>
+					</div>
+
                   </form>
-            </div>
+			</div>
+			</div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+			<button type="button" class="btn btn-primary">Odeslat</button>
+              <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
+      
+            </div>
+          </div>
+        </div>
+	  </div>
+	  
+
+
+	   <div class="modal fade" id="orderModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">OBJEDNÁVKOVÝ FORMULÁR</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+			</div>
+			<div class="container">
+            <div class="modal-body">
+                <form>
+					<label for="inputEmail4">NEZÁVAZNÝ VÝPOčET CENY</label>
+
+					<div class="form-group">
+						<label for="usr">jméno a příjmení:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+
+					<div class="form-group">
+						<label for="usr">ulice:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+
+					<div class="form-group">
+						<label for="usr">město:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+
+					<div class="form-group">
+						<label for="usr">PSČ:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+
+					<div class="form-group">
+						<label for="usr">kontaktní telefon:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+
+					<div class="form-group">
+						<label for="usr">kontaktní e-mail:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+
+					<label for="inputEmail4">TYP PRSTENU</label>
+
+					<div class="form-group">
+						<label for="usr">dámský:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+					<div class="form-group">
+						<label for="usr">pánský:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+
+					<label for="inputEmail4">VELIKOST PRSTENU</label>
+
+					<div class="form-group">
+						<label for="usr">dámský:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+					<div class="form-group">
+						<label for="usr">pánský:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+                    <div class="form-row">
+					
+                      <div class="form-group col-md-6">
+                        <label for="inputEmail4">jméno a příjmení</label>
+                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                      </div>
+                      <div class="form-group col-md-6">
+                        <label for="inputPassword4">e-mail nebo telefon:
+						</label>
+                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                      </div>
+					</div>
+
+					
+					<label for="inputEmail4">TYP KAMENE</label>  
+					<div class="form-check">
+						<label class="form-check-label">
+								<input type="radio" class="form-check-input" name="optradio">zirkon
+
+						</label>
+					</div>
+					<div class="form-check">
+							<label class="form-check-label">
+								<input type="radio" class="form-check-input" name="optradio">briliant
+							</label>
+					</div>
+					<div class="form-group">
+						<label for="usr">počet kamenů:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+					<br/>
+					<label for="inputEmail4">MATERIÁL</label>  
+					<div class="form-check">
+						<label class="form-check-label">
+								<input type="radio" class="form-check-input" name="optradio">zlato 14kt
+
+						</label>
+					</div>
+					<div class="form-check">
+							<label class="form-check-label">
+								<input type="radio" class="form-check-input" name="optradio">stříbro
+							</label>
+					</div>
+					
+					<br/>
+					<label for="inputEmail4"> ZLATO, BARVA</label>  
+					<div class="form-check">
+						<label class="form-check-label">
+								<input type="radio" class="form-check-input" name="optradio">bílé
+
+
+						</label>
+					</div>
+					<div class="form-check">
+							<label class="form-check-label">
+								<input type="radio" class="form-check-input" name="optradio">žluté
+							</label>
+					</div>
+					<div class="form-check">
+							<label class="form-check-label">
+								<input type="radio" class="form-check-input" name="optradio">červené
+							</label>
+					</div>
+
+					<label for="inputEmail4"> TEXT RYTINY DO PRSTENU</label>  
+
+					<div class="form-group">
+						<label for="usr">dámský:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+
+						<div class="form-group">
+						<label for="usr">pánský:</label>
+						<input type="text" class="form-control" id="usr">
+					</div>
+
+					<label for="inputEmail4">ZPůSOB PřEVZTÍ</label>  
+					<div class="form-check">
+						<label class="form-check-label">
+								<input type="radio" class="form-check-input" name="optradio">Česká pošta dobírka
+						</label>
+					</div>
+					<div class="form-check">
+							<label class="form-check-label">
+								<input type="radio" class="form-check-input" name="optradio">osobně na adrese provozovny
+							</label>
+					</div>
+					<span> Objednávka telefonicky: 732 450 054</span>
+				
+					<div class="form-group">
+						<label for="message-text" class="col-form-label">Vaše poznámky, vlastní úpravy:</label>
+						<textarea class="form-control" id="message-text"></textarea>
+					</div>
+					
+
+                  </form>
+			</div>
+			</div>
+            <div class="modal-footer">
+			<button type="button" class="btn btn-primary">Odeslat</button>
+              <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
+      
             </div>
           </div>
         </div>
