2223a0db60a486929e465c53f43e1bd973047be4
hamely <sarmiento101147@gmail.com>
2019-08-23 13:02:24 -0500

    list order

--- application/language/czech/backend_lang.php
+++ application/language/czech/backend_lang.php
@@ -59,3 +59,25 @@ $lang['b_pages_label_meta_keywords'] = "meta_keywords";
 $lang['b_pages_label_meta_description'] = "meta_description";
 $lang['b_pages_label_meta_image'] = "meta_image";
 $lang['b_pages_label_created_at'] = "created_at";
+
+//list order
+
+$lang['b_orders_label_name'] = "name";
+$lang['b_orders_label_address'] = "address";
+$lang['b_orders_label_city'] = "city";
+$lang['b_orders_label_zip_code'] = "zip_code";
+$lang['b_orders_label_contact_telephone'] = "contact_telephone";
+$lang['b_orders_label_contact_email'] = "contact_email";
+$lang['b_orders_label_type_male'] = "type_male";
+$lang['b_orders_label_type_female'] = "type_female";
+$lang['b_orders_label_size_male'] = "size_male";
+$lang['b_orders_label_size_female'] = "size_female";
+$lang['b_orders_label_type_stone'] = "type_stone";
+$lang['b_orders_label_num_stone'] = "num_stone";
+$lang['b_orders_label_material'] = "material";
+$lang['b_orders_label_color'] = "color";
+$lang['b_orders_label_text_engraving_male'] = "text_engraving_male";
+$lang['b_orders_label_text_engraving_female'] = "text_engraving_female";
+$lang['b_orders_label_'] = "takeover_method";
+$lang['b_orders_label_notes'] = "notes";
+$lang['b_orders_label_created_at'] = "created_at";
